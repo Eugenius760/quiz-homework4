288771e57031be28cb71c43a4fddb3c94e4fc7ee
Darryl <williams.de39@gmail.com>
2020-03-25 19:47:38 -0700

    added script

---
+++ script.js
@@ -0,0 +1,6 @@
+var startEl = document.querySelector("start-page")
+var startBtn = document.getElementById("start-button")
+
+startBtn.addEventListener("click", function() {
+    console.log("1")
+})
