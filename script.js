22ff5f6926a32834b28c7f954d9d447e8b087f9d
Darryl <williams.de39@gmail.com>
2020-03-28 22:42:48 -0700

    timer and clear

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 var startEl = document.querySelector("start-page")
 var startBtn = document.getElementById("start-button")
+var timer = document.getElementById("seconds")
+// var display = document.querySelector("display")
 
-startBtn.addEventListener("click", function() {
+var ttlSecs = 10
+
+function secondsLeft() {
+    var timerInterval = setInterval(function() {
+        ttlSecs--;
+        timer.textContent = ttlSecs + " seconds left";
+
+        if(ttlSecs === 0) {
+            clearInterval(timerInterval);
+            endQuiz()
+        }
+    }, 1000);
+}
+
+function endQuiz() {
+    var testEl = document.createElement("p")
+
+    testEl.setAttribute = "test"
+}
+
+secondsLeft()
+endQuiz()
+
+startBtn.addEventListener("change", function() {
+    // var clear = document.getElementById("display")
+    // clear.innerHTML = "test";
+    var questOne = document.createElement("div")
+    questOne.innerHTML = "test";
     console.log("1")
-})
+});
